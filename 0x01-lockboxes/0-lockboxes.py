ebea0c4d0be92fa3b60857d8d07d88fc1ec56edc
Talabi Rebecca <122875790+TemiladeRebecca@users.noreply.github.com>
2024-01-11 13:20:00 +0100

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """
-Interviw question that unlocks boxes, each box contains a list of keys
-that can open other boxes
+This method unlocks boxes, there are a number of boxes and each box numbered sequentially.
+Each box contains keys to other boxes
 """
 
 
 def canUnlockAll(boxes):
-    """Fuction to unlock all boxes"""
+    """Function to unlock all boxes"""
     unlocked = [0]
 
     for box_index, box_elements in enumerate(boxes):
         # Enumerates and returns box indices and their corresponding
         # elements which are keys in this case
         if not box_elements:
-            continue        # if the box has no keys/empty, we continue
+            continue        # if the box has no keys or is empty
 
         for key in box_elements:
             if key < len(boxes) and key != box_index and key not in unlocked:
                 unlocked.append(key)
-                # this step unlocks the box with the key if conditions are met
+                # the box is unlocked only if conditions are met
 
     if len(unlocked) == len(boxes):
         return True
