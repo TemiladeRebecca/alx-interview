13241ad4f9b7a01bd07e46dbb91600f0fe827ebd
Talabi Rebecca <122875790+TemiladeRebecca@users.noreply.github.com>
2024-01-11 13:34:24 +0100

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 """
-This method unlocks boxes, there are a number of boxes and each box numbered sequentially
-Each box contains keys to other boxes
+Interview question that unlocks boxes, each box contains a list of keys
+that can open other boxes
 """
 
+
 def canUnlockAll(boxes):
-    '''Checks if all the boxes in a list of boxes containing the keys
-    (indices) to other boxes can be unlocked given that the first
-    box is unlocked.
-    '''
-    n = len(boxes)
-    seen_boxes = set([0])
-    unseen_boxes = set(boxes[0]).difference(set([0]))
-    while len(unseen_boxes) > 0:
-        boxIdx = unseen_boxes.pop()
-        if not boxIdx or boxIdx >= n or boxIdx < 0:
-            continue
-        if boxIdx not in seen_boxes:
-            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
-            seen_boxes.add(boxIdx)
-    return n == len(seen_boxes)
+    """Function to unlock all boxes"""
+    unlocked = [0]
+
+    for box_index, box_elements in enumerate(boxes):
+        # Enumerates and returns box indices and their corresponding
+        # elements which are keys in this case
+        if not box_elements:
+            continue        # if the box has no keys/empty, we continue
+
+        for key in box_elements:
+            if key < len(boxes) and key != box_index and key not in unlocked:
+                unlocked.append(key)
+                # unlocks boxes only if conditions are met
+
+    if len(unlocked) == len(boxes):
+        return True
+    return False
